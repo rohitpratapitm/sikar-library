7c59b67978f0ddf1bdd4c4a6d8ac7dadce708a09
rohitpratapitm <rohitpratapitm@gmail.com>
2017-12-01 19:54:57 -0500

    test getAccessToken

--- src/app/components/common/login/login.component.ts
+++ src/app/components/common/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService, OAuth2Service } from '../services/all';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
     selector: 'login',
@@ -9,15 +10,21 @@ import { AuthenticationService, OAuth2Service } from '../services/all';
 export class LoginComponent implements OnInit {
 
     constructor(private authenticationService: AuthenticationService,
-        private oAuth2Service: OAuth2Service) {
+        private oAuth2Service: OAuth2Service,
+        private router: ActivatedRoute) {
     }
 
     ngOnInit() {
 
+        this.router.queryParams.subscribe((queryParams: Params) => {
+            let code: string = queryParams['code'];
+            if (code) {
+                this.oAuth2Service.getAccessToken(code);
+            }
+        });
     }
 
     routeToAuthorisationServer(): void {
-        this.oAuth2Service.initiateAuthorizationFlow();
-        // this.authenticationService.initiateAuthorizationFlow();
+        this.oAuth2Service.getAuthorizationURL();
     }
 }
--- src/app/components/common/services/authorisation/oauth2.service.ts
+++ src/app/components/common/services/authorisation/oauth2.service.ts
@@ -1,53 +1,65 @@
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http/src/response';
+import { Params } from '@angular/router';
+import { error } from 'util';
 
+/**
+ * Authorisation flow
+ */
 @Injectable()
 export class OAuth2Service  {
 
-    private consumer_key: string = 'dj0yJmk9Yjdsek5yV01FakJGJmQ9WVdrOWRUbGlXR2RuTTJVbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0yMA--';
-    private consumer_secret: string = 'db2f69ad9c2d7ac4e3fa53f9c2947e1b6a4f8e44';
-    private url: string = 'https://api.login.yahoo.com/oauth2/request_auth';
-    private signature_method: string = 'PLAINTEXT';
-    private signature_method_hma: string = 'HMAC-SHA1';
-    private redirect_uri: string = 'sikar-library.herokuapp.com';
+    private readonly consumer_key: string = 'dj0yJmk9Yjdsek5yV01FakJGJmQ9WVdrOWRUbGlXR2RuTTJVbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0yMA--';
+    private readonly consumer_secret: string = 'db2f69ad9c2d7ac4e3fa53f9c2947e1b6a4f8e44';
+    private readonly AUTHORIZATION_URL: string = 'https://api.login.yahoo.com/oauth2/request_auth';
+    private readonly ACCESS_TOKEN_URL: string = 'https://api.login.yahoo.com/oauth2/get_token';
+    private readonly signature_method: string = 'PLAINTEXT';
+    private readonly signature_method_hma: string = 'HMAC-SHA1';
+    private readonly redirect_uri: string = 'http://sikar-library.herokuapp.com/login';
+    private readonly QUERY_PARAM_SEPARATOR: string = '?';
 
     constructor(private http: HttpClient) {
       
     }
 
-    public initiateAuthorizationFlow() {
-        // Authorisation flow
-        // 1. Get an authorization URL and authorize access
-        this.authorizeAccess().subscribe(response => {
+    // 1. Get an Authorization URL and authorize access
+    public getAuthorizationURL(): string {
+        
+        return this.AUTHORIZATION_URL
+        .concat(this.QUERY_PARAM_SEPARATOR).concat('client_id', this.consumer_key)
+        .concat(this.QUERY_PARAM_SEPARATOR).concat('redirect_uri', this.redirect_uri)
+        .concat(this.QUERY_PARAM_SEPARATOR).concat('response_type', 'code')
+        .concat(this.QUERY_PARAM_SEPARATOR).concat('language', 'en-us');
+    }
+
+    // 2. GET an ACCESS TOKEN
+    public getAccessToken(code: string): void {
+
+        // create header
+        const headers: HttpHeaders = new HttpHeaders()
+        .append('Authorization', 'Basic '.concat(btoa(this.consumer_key.concat(':').concat(this.consumer_secret))));
+
+        // construct body
+        const params: HttpParams = new HttpParams()
+        .append('grant_type', 'authorization_code')
+        .append('redirect_uri', this.redirect_uri)
+        .append('code', code);
+
+        this.http.post(this.ACCESS_TOKEN_URL, params, {
+            headers: headers, responseType: 'text'
+        }).subscribe(response => {
             if (response) {
-                console.log('Response is: ' + response);
-            } else {
-                console.log('Could not retrieve token data');
+                console.log(' Response is : ' + response);
             }
-        });
-    }
-    private authorizeAccess(): Observable<any> {
-        
-          // Only support GET in this function
-          const method = 'GET';
-
-          // Params need to be sorted by key
-          const params: HttpParams = new HttpParams()
-          .set('client_id', this.consumer_key)
-          .set('redirect_uri', 'http://localhost:8080')
-          .set('response_type', 'code')
-          .set('language', 'en-us');
-
-        
-        return this.http.get(this.url, { params: params, responseType: 'text'});
+        }, ((err: HttpErrorResponse) => this.handleError(err)));
     }
 
-    private handleError(error: HttpErrorResponse): void {
-        if (error.error instanceof Error) {
+    private handleError(err: HttpErrorResponse): void {
+        if (err.error instanceof Error) {
             // A client-side or network error occurred. Handle it accordingly.
-            console.log('An error occurred:', error.error.message);
+            console.log('An error occurred:', err.error.message);
           } else {
             // The backend returned an unsuccessful response code.
             // The response body may contain clues as to what went wrong,
