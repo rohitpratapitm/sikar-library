304b9f74c1b28cfb1d694e7c5425037ccf29830e
rohitpratapitm <rohitpratapitm@gmail.com>
2017-12-16 19:18:42 -0500

    Fixed production file

---
+++ src/environments/environment.dev.prod.ts
@@ -0,0 +1,11 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: true,
+  key: 'dj0yJmk9OGx6NHc0cHFUQUY3JmQ9WVdrOVRVSnFlalpuTkRJbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1iZA--',
+  secret: 'f6a46e52a8d3b3b134e8f4bccd3d25a700d3dc50',
+  redirect_uri: 'http://localtest.me/login'
+};
